95698ea91b84ba770eaa9a3f783d8b8a0474aeae
Nicola Clark <nicola@slottedspoon.dev>
2023-05-27 18:33:56 -0600

    basic http server

--- times.ts
+++ times.ts
@@ -1,1 +1,11 @@
-console.log("howdy! i'm times! :)");
+import { serve } from 'https://deno.land/std@0.189.0/http/server.ts';
+
+function webHandler(_req: Request): Response {
+  return new Response('howdy! :)', { status: 200 });
+}
+
+const port = parseInt(Deno.env.get('PORT') || '8080');
+
+console.log('howdy! i\'m times! :)');
+console.log('times is listening on port ', port);
+await serve(webHandler, { port });
