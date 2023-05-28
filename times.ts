b2c5b5ae437518644855d95bcc5702a7f599ecd1
Nicola Clark <nicola@slottedspoon.dev>
2023-05-27 18:58:00 -0600

    moved http to oak

--- times.ts
+++ times.ts
@@ -1,11 +1,12 @@
-import { serve } from 'https://deno.land/std@0.189.0/http/server.ts';
+import { Application } from 'https://deno.land/x/oak@v12.5.0/mod.ts';
 
-function webHandler(_req: Request): Response {
-  return new Response('howdy! :)', { status: 200 });
-}
-
+const app = new Application();
 const port = parseInt(Deno.env.get('PORT') || '8080');
+
+app.use(function (ctx) {
+  ctx.response.body = 'howdy! :)';
+});
 
 console.log('howdy! i\'m times! :)');
 console.log('times is listening on port ', port);
-await serve(webHandler, { port });
+await app.listen({ port });
